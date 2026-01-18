34130b677d27284e350313db3d793487dbc2c310
BrentTanKian <brentyly@gmail.com>
2026-01-18 20:00:02 +0800

    Day 20 solutions

---
+++ 2015/Day 20/2015_Day20_part1.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    houses = 3400000
+    house_array = [0 for i in range(houses)]
+    for elf in range(1, houses + 1):
+        multiplier = 1
+        while (elf * multiplier) - 1 < houses:
+            house_array[(elf * multiplier) - 1] += (elf * 10)
+            multiplier += 1
+    for i in range(len(house_array)):
+        if house_array[i] >= 34000000:
+            print(i + 1)
+            break
